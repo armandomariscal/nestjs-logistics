a0fd53b48b55409dd7ea0ef0144ef2d97aba1f7b
Armando Mariscal <armando.marisscal@gmail.com>
2026-02-19 23:41:19 -0600

    feat: Setup PostgreSQL with Docker and pgAdmin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,46 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+
+      host: process.env.POSTGRES_HOST || 'localhost',
+
+      port: parseInt(
+        process.env.POSTGRES_PORT || '5432'
+      ),
+
+      username: process.env.POSTGRES_USER,
+
+      password: process.env.POSTGRES_PASSWORD,
+
+      database: process.env.POSTGRES_DB,
+
+      autoLoadEntities: true,
+
+      synchronize: true,
+    }),
+
+    OrdersModule,
+
+  ],
+
   controllers: [AppController],
+
   providers: [AppService],
+
 })
 export class AppModule {}
